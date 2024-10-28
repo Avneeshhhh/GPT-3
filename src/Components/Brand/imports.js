68152c3a80841d1346afe02b7928ed5949798d83
Avneesh Yadav <avneeshyadav0115@gmail.com>
2024-10-28 21:38:55 +0530

    Inizilize Commit

---
+++ src/Components/Brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../Assets/google.png';
+import slack from '../../Assets/slack.png';
+import atlasssian from '../../Assets/atlassian.png';
+import dropbox from '../../Assets/dropbox.png';
+import shopify from '../../Assets/shopify.png';
+
+export{
+    google,
+    slack,
+    atlasssian,
+    dropbox,
+    shopify,
+}
